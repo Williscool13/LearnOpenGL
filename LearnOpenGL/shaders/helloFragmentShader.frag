bae03d0a08ac7e277a4e0325c30210455a19d2f5
Williscool13 <twtw40@gmail.com>
2024-01-23 00:11:42 +0700

    multile triangles, element buffer object (indices), multi vao, new input system, cleaned up code

--- LearnOpenGL/shaders/helloFragmentShader.frag
+++ LearnOpenGL/shaders/helloFragmentShader.frag
@@ -1,7 +1,26 @@
 #version 330 core
+in vec3 vColor;
 out vec4 color;
+uniform float time;
+
+vec3 gammaCorrect(vec3 color, float gamma)
+{
+	return pow(color, vec3(1.0f / gamma));
+}
+
 
 void main()
 {
-    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    float r = sin(time) * 0.5 + 0.5;
+    float g = cos(time) * 0.5 + 0.5;
+    float b = sin(time) * cos(time) * 0.5 + 0.5;
+    float col = (sin(time) / 1.5) + 0.5;
+    //col = clamp(col, 0.0f, 1.0f);
+    //vec3 albedo = vColor * col;
+
+    vec3 albedo = vec3(r * vColor.r, g * vColor.g, b * vColor.b);
+
+    vec3 correctedColor = gammaCorrect(albedo, 2.2f);
+
+    color = vec4(correctedColor, 1.0f);
 } 
--- LearnOpenGL/src/HelloWindow.cpp
+++ LearnOpenGL/src/HelloWindow.cpp
@@ -1,9 +1,18 @@
-#include <windows.h>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
+
+#include <windows.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <ext/glext.h>
+#include <wingdi.h>
+
+
+
+bool first = true;
+GLuint VAOs[2];
 
 std::string readShaderFile(const char* filePath) 
 {
@@ -58,59 +67,198 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height); 
 }
 
-/// <summary>
-/// Process Inputs of the user. 
-/// If the user presses the escape key, the window should close.
-/// </summary>
-/// <param name="window"></param>
-void processInput(GLFWwindow* window)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) 
-		glfwSetWindowShouldClose(window, true); 
+	if (key == GLFW_KEY_1 && action == GLFW_PRESS) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+
+	if (key == GLFW_KEY_2 && action == GLFW_PRESS) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+
+	if (key == GLFW_KEY_3 && action == GLFW_PRESS) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+	}
+
+	if (key == GLFW_KEY_4 && action == GLFW_PRESS) {
+		first = !first;
+	}
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
 
-/// <summary>
-/// Renders the scene.
-/// Animates the background color of the window.
-/// </summary>
-void renderScene(GLuint program) {
+GLuint setupShader(GLenum shaderType, const char* shaderSource) {
+	// compile vertex shader
+	GLuint shader;
+	shader = glCreateShader(shaderType);
+	glShaderSource(shader, 1, &shaderSource, NULL);
+	glCompileShader(shader);
+	// check if vertex shader compiled successfully
+	checkShaderCompileStatus(shader);
+	return shader;
+}
 
+void setupVertexArrayObjectOne(GLuint program) {
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	-0.1,  -0.1f, 0.0f,  // top right
+	-0.1f, -0.9f, 0.0f,  // bottom right
+	-0.9f, -0.9f, 0.0f,  // bottom left
+	-0.9f, -0.1f, 0.0f   // top left 
 	};
 
-	// Vertex Array Object
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+	float colors[] = {
+	 1.0f,  0.0f, 0.0f,  // top right
+	 0.0f,  1.0f, 0.0f,  // bottom right
+	 0.0f,  0.0f, 1.0f,  // bottom left
+	 1.0f,  1.0f, 0.0f   // top left 
+	};
 
-	// Vertex Buffer Object
-	GLuint buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	unsigned int indices[] = {
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	// Bind Vertex Array Object (VAO)
+	glBindVertexArray(VAOs[0]);
+
+
+	GLuint VBOs[2], EBO;
+	glGenBuffers(2, VBOs);
+
+	/// VBOs must be bound before glVertexAttribPointer is called
+	// Set Up Points Vertex Buffer Object (VBO)
+	glGenBuffers(1, &VBOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// get attribute location
 	GLuint pos = glGetAttribLocation(program, "pos");
-	// set attribute pointer
-	glEnableVertexAttribArray(pos);
-	// tell OpenGL how to interpret the vertex data (per vertex attribute), it uses the previously defined VBO for its data
 	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	glEnableVertexAttribArray(pos);
 
-	// draw triangle
+	// Set Up Colors Vertex Buffer Object (VBO)
+	glGenBuffers(1, &VBOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+	GLuint color = glGetAttribLocation(program, "color");
+	glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	glEnableVertexAttribArray(color);
+
+
+	// Element (Index) Buffer Object (EBO)
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+void setupVertexArrayObjectTwo(GLuint program) {
+	float verticesTwo[] = {
+		-0.5f, 0.0f, 0.0f,  // bottom left
+		0.5f,  0.0f, 0.0f,  // bottom right
+		0.0f,  0.5f, 0.0f,  // middle top
+		0.0f, -0.5f, 0.0f   // middle bottom
+	};
+
+	float colorsTwo[] = {
+		1.0f,  0.0f, 0.0f,  // top right
+		0.0f,  1.0f, 0.0f,  // bottom right
+		0.0f,  0.0f, 1.0f,  // bottom left
+		1.0f,  1.0f, 0.0f   // top left 
+	};
+	
+	unsigned int indices[] = {  
+		0, 1, 2,   // first triangle
+		0, 1, 3    // second triangle
+	};
+
+	// Bind Vertex Array Object (VAO)
+	glBindVertexArray(VAOs[1]);
+
+	GLuint VBOs[2], EBO;
+
+	/// VBOs must be bound before glVertexAttribPointer is called
+	// Set Up Points Vertex Buffer Object (VBO)
+	glGenBuffers(1, &VBOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesTwo), verticesTwo, GL_STATIC_DRAW);
+
+	GLuint pos = glGetAttribLocation(program, "pos");
+	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	glEnableVertexAttribArray(pos);
+
+	// Set Up Colors Vertex Buffer Object (VBO)
+	glGenBuffers(1, &VBOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colorsTwo), colorsTwo, GL_STATIC_DRAW);
+
+	GLuint color = glGetAttribLocation(program, "color");
+	glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	glEnableVertexAttribArray(color);
+
+
+	// Element (Index) Buffer Object (EBO)
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+
+void setupProgram(GLuint program) {
+	// Vertex Shader
+	GLuint vertexShader = setupShader(GL_VERTEX_SHADER, readShaderFile("shaders\\helloVertexShader.vert").c_str());
+
+	// Fragment Shader
+	GLuint fragmentShader = setupShader(GL_FRAGMENT_SHADER, readShaderFile("shaders\\helloFragmentShader.frag").c_str());
+
+	// Shader Program
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+	checkShaderLinkStatus(program);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	glGenVertexArrays(2, VAOs);
+	setupVertexArrayObjectOne(program);
+	setupVertexArrayObjectTwo(program);
+
+	// Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+	glBindVertexArray(0);
+}
+
+void renderScene(GLuint program) {
+	GLuint timeUniform = glGetUniformLocation(program, "time");
+	
+	// draw in wireframe mode
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// Choose program and vertex array object
 	glUseProgram(program); 
-	glDrawArrays(GL_TRIANGLES, 0, 3); 
+	if (first) {
+		glBindVertexArray(VAOs[0]);
+	}
+	else {
+		glBindVertexArray(VAOs[1]);
+	}
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	glUniform1f(timeUniform, glfwGetTime());
+
 	glUseProgram(0);
+    glBindVertexArray(0);
 
 
 	// animate and change background color
-	float timeValue = glfwGetTime();
+	/*float timeValue = glfwGetTime();
 	float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
 	float redValue = (cos(timeValue) / 2.0f) + 0.5f;
 	float blueValue = (sin(timeValue) / 2.0f) + (cos(timeValue) / 2.0f);
-	glClearColor(redValue, greenValue, blueValue, 1.0f);
+	glClearColor(redValue, greenValue, blueValue, 1.0f);*/
 }
 
 int main()
@@ -148,59 +296,23 @@ int main()
 	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 
 
-	// Vertex Shader
-	std::string vertexShaderSourceString = readShaderFile("shaders\\helloVertexShader.vert");
-	const GLchar* vertexShaderSource = vertexShaderSourceString.c_str();
-	//// compile vertex shader
-	GLuint vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	//// check if vertex shader compiled successfully
-	checkShaderCompileStatus(vertexShader);
-
-	// Fragment Shader
-	std::string fragmentShaderSourceString = readShaderFile("shaders\\helloFragmentShader.frag");
-	const GLchar* fragmentShaderSource = fragmentShaderSourceString.c_str();
-	//// compile fragment shader
-	GLuint fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	//// check if fragment shader compiled successfully
-	checkShaderCompileStatus(fragmentShader);
-
-
-	// Shader Program
 	GLuint shaderProgram;
 	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	//// check if shader program linked successfully
-	checkShaderLinkStatus(shaderProgram);
-
-	// delete shaders
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	// use shader program
-	glUseProgram(shaderProgram);
-
-
+	setupProgram(shaderProgram);
+	
+	glfwSetKeyCallback(window, key_callback);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		processInput(window);
-		
+
 		// rendering commands here
 		renderScene(shaderProgram);
 
-		// check and call events and swap the buffers
-		glfwSwapBuffers(window);
+		// poll events and swap the buffers
 		glfwPollEvents();
+		glfwSwapBuffers(window);
 	}
 
 	// clean up
